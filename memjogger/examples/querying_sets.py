4813a21f5cfd69594f41f51ba16be810f3bf6d8a
Aleksander Łukasz <alukasz@alukasz.com>
2013-01-17 13:16:07 +0100

    Updating usage example

--- memjogger/examples/querying_sets.py
+++ memjogger/examples/querying_sets.py
@@ -4,5 +4,11 @@ from memjogger.api import Handle
 if __name__ == '__main__':
     api = Handle(email = sys.argv[1], password = sys.argv[2])
     result = api.get_card_sets()
+    cs = None
     for cs in result.data['card_sets']:
         print cs['id'], cs['name']
+    
+    if cs:
+        result = api.get_card_set(cs['id'])
+        cs = result.data['card_set']
+        print 'Last set:' , cs['id'], cs['name']
