6ec761e0e213c7ce01fac1c8fa9f2ea15a9192ed
Aleksander Łukasz <alukasz@alukasz.com>
2013-01-17 12:21:32 +0100

    Authentication and getting card sets

---
+++ memjogger/api.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+class Response:
+    def __init__(self, http, data = None):
+        self.http = http
+        self.data = data
+        
+class ErrorResponse(Response):
+    pass
+
+class MemjoggerAuth(requests.auth.AuthBase):
+    def __init__(self, token = ''):
+        self.token = token
+    def __call__(self, request):
+        if self.token:
+            request.headers['X-Memjogger-Token'] = self.token
+        return request
+    def __repr__(self):
+        return 'MemjoggerAuth: ' + self.token
+        
+API_URL = 'http://memjogger.com/api/'
+
+class Handle:
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.auth = MemjoggerAuth()
+    
+    def authenticate(self):
+        response = requests.post(API_URL + 'user/login', data = json.dumps(dict(email = self.email, passwd = self.password)))
+        if response.status_code == 200:
+            self.auth.token = json.loads(response.text)['token']
+    
+    def get_card_sets(self):
+        def call():
+            return requests.get(API_URL + 'cardset', auth = self.auth)
+        
+        response = call()
+        if response.status_code == 401:
+            self.authenticate()
+            response = call()
+        if response.status_code == 401:
+            return ErrorResponse(response)
+        
+        return Response(response, json.loads(response.text))
+        
+    
--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(
     name='memjogger',
     version='0.0.1',
     packages=find_packages(),
+    install_requires = ['nose', 'requests', 'mock']
 )
